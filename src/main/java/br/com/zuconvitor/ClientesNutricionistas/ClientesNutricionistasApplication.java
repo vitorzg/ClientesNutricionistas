56ba9cfe16adc6d3e617d15e01cf244f5c4513e3
Vitor Zucon <vitor.zucon25@gmail.com>
2024-04-01 20:30:10 -0300

    first config

---
+++ src/main/java/br/com/zuconvitor/ClientesNutricionistas/ClientesNutricionistasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zuconvitor.ClientesNutricionistas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesNutricionistasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesNutricionistasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zuconvitor/ClientesNutricionistas/ClientesNutricionistasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zuconvitor.ClientesNutricionistas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesNutricionistasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
